5c1d4be426af1279034468176507a7d7d19d6e9f
Sarthak Bakre <sarthak.bakre@gmail.com>
2025-01-03 22:12:47 +0530

    feat: fixed dimentions, improved wasm files

--- src/components/valueController.tsx
+++ src/components/valueController.tsx
@@ -1,41 +1,53 @@
-import { useState } from 'react';
+import { useState } from "react";
 import "./value_controller.css";
+import { colors } from "../constants/theme";
 
 interface ValueControllerProps {
-    onChange: (arg0: number) => void;
-    minValue: number,
-    maxValue: number,
-    initialValue: number,
+  onChange: (arg0: number) => void;
+  minValue: number;
+  maxValue: number;
+  initialValue: number;
 }
 
-function ValueController({ onChange, minValue, maxValue, initialValue }: ValueControllerProps) {
-    const [count, setCount] = useState(initialValue);
+function ValueController({
+  onChange,
+  minValue,
+  maxValue,
+  initialValue,
+}: ValueControllerProps) {
+  const [count, setCount] = useState(initialValue);
 
-    const increment = () => {
-        let newValue = Math.min(count + 10, maxValue);
-        if (newValue == count) {
-            return;
-        }
-        setCount(newValue);
-        onChange(newValue);
-    };
+  const increment = () => {
+    let newValue = Math.min(count + 10, maxValue);
+    if (newValue == count) {
+      return;
+    }
+    setCount(newValue);
+    onChange(newValue);
+  };
 
-    const decrement = () => {
-        let newValue = Math.max(minValue, count - 10);
-        if (newValue == count) {
-            return;
-        }
-        setCount(newValue);
-        onChange(newValue);
-    };
+  const decrement = () => {
+    let newValue = Math.max(minValue, count - 10);
+    if (newValue == count) {
+      return;
+    }
+    setCount(newValue);
+    onChange(newValue);
+  };
 
-    return (
-        <div className="counter-container">
-            <button className="counter-button" onClick={decrement}>-</button>
-            <span>{count}</span>
-            <button className="counter-button" onClick={increment}>+</button>
-        </div>
-    );
+  return (
+    <div className="counter-container">
+      <button className="counter-button" onClick={decrement} style={{backgroundColor: colors.primary}}>
+        -
+      </button>
+      <div className="counter-text" style={{backgroundColor: colors.secondary}}>
+        <span style={{color: colors.textPrimary}}>{count}</span>
+      </div>
+      <button className="counter-button" onClick={increment} style={{backgroundColor: colors.primary}}>
+        +
+      </button>
+    </div>
+  );
 }
 
 export default ValueController;
